ddf28a825ea665bfd76e9a263521f8cd5899ed0b
jjardel <jjardel@square-root.com>
2016-12-21 08:29:58 -0500

    download trump tweets

--- etl/extract/_extractors.py
+++ etl/extract/_extractors.py
@@ -5,7 +5,9 @@ from common.util import get_path
 from common.lw import get_logger, get_root_logger
 from zipfile import ZipFile
 from io import BytesIO
-import re, os
+from pandas.io.json import json_normalize
+
+import tweepy, json, re, os
 
 
 class EventExtractor(object):
@@ -29,14 +31,19 @@ class EventExtractor(object):
         """
         Get links to files on S3 from Phoenix home page
 
+        :returns list of hyperlinks to data file locations on S3
         """
+
+        file_links = []
 
         page = get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
         links = soup.find_all(href=re.compile(self.base))
         for link in links:
-            self.file_links.append(link.contents[0])
+            file_links.append(link.contents[0])
+
+        return file_links
 
     def extract(self):
         """
@@ -44,9 +51,9 @@ class EventExtractor(object):
 
         """
 
-        self._get_files()
+        files = self._get_files()
 
-        for link in self.file_links:
+        for link in files:
             file_name = link.replace(self.base, '').replace('.zip', '')
             file_path = self.data_path.format(file_name)
 
@@ -61,10 +68,79 @@ class EventExtractor(object):
             z = ZipFile(BytesIO(r.content))
             z.extractall(self.data_path.format('.'))
 
+
+class TweetExtractor(object):
+
+    def __init__(self):
+
+        # initialize variables
+        self.user = 'realdonaldtrump'
+
+        self.loc = get_path(__file__) + '/../{0}'
+        self.logger = get_logger(__name__)
+
+        # authenticate to the API
+        self._initialize_api()
+
+    def _initialize_api(self):
+        """
+        Handles authentication with Twitter API using tweepy.
+        Requires a file at config/twitter_creds.json with the following attributes:
+
+            "access_token":
+            "access_secret":
+            "consumer_token":
+            "consumer_secret":
+
+        See Twitter OAUTH docs + Tweepy docs for more details http://docs.tweepy.org/en/v3.5.0/auth_tutorial.html
+        :return:
+        """
+
+        with open(self.loc.format('../config/twitter_creds.json')) as fp:
+            config = json.load(fp)
+
+        auth = tweepy.OAuthHandler(config['consumer_token'], config['consumer_secret'])
+        auth.set_access_token(config['access_token'], config['access_secret'])
+        self.logger.info('Successfully Authenticated to Twitter API')
+
+        self.api = tweepy.API(auth)
+
+    def _export_to_csv(self, df):
+
+        file_base = self.loc.format('data/tweets.csv')
+
+        self.logger.info('Writing Trump tweets to {0}'.format(file_base))
+        df.to_csv(file_base)
+
+    def extract(self, batch_size=200, num_tweets=2000):
+
+        res = []
+
+        self.logger.info('Collecting the last {0} Trump tweets'.format(num_tweets))
+        cursor = 0
+        page = 1
+
+        while cursor <= num_tweets:
+            res += self.api.user_timeline(self.user, count=batch_size, page=page)
+            page += 1
+            cursor += batch_size
+
+        # pick off the schema from the first tweet
+        df = json_normalize(res[0]._json)
+        df.drop(0, inplace=True)
+
+        for tweet in res:
+            df = df.append(json_normalize(tweet._json))
+
+        # write tweets to disk
+        self._export_to_csv(df)
+
 if __name__ == '__main__':
 
     lg = get_root_logger(filename='log.log')
 
-    ex = EventExtractor()
-    ex.extract()
+    #ex = EventExtractor()
+    #ex.extract()
+    twex = TweetExtractor()
+    twex.extract()
     print('test')
