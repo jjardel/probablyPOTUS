71bf8a4684b3e7a2d379c313e3c1fcc462311a4a
jjardel <johnjardel@gmail.com>
2017-01-29 23:35:41 -0600

    updating features

--- etl/transform/002_create_features.sql
+++ etl/transform/002_create_features.sql
@@ -35,6 +35,7 @@ c2 AS(
         array_length(string_to_array(text, '!'), 1) - 1 AS num_exclamation_points,
         length(text) AS num_characters,
         array_length(regexp_split_to_array(text, '([A-Z][\w-]*(\s+[A-Z][\w-]*)+)'), 1) - 1 AS num_uppercase_strings,
+        CASE WHEN text LIKE '"@' OR text LIKE 'RT%' THEN 1 ELSE 0 END AS is_trump_retweet,   -- he often RTs lots of tweets in a row, and starts by copying the text of the tweet
         LAG(created_at, 1) OVER w AS timestamp_last_tweet,
         LEAD(created_at, 1) OVER w AS timestamp_next_tweet
     FROM c1
@@ -59,9 +60,10 @@ SELECT
     num_exclamation_points,
     num_characters,
     num_uppercase_strings,
-    CASE WHEN (created_at - timestamp_last_tweet < '5 minutes' OR timestamp_next_tweet - created_at < '5 minutes')
+    is_trump_retweet,
+    CASE WHEN (created_at - timestamp_last_tweet < '10 minutes' OR timestamp_next_tweet - created_at < '10 minutes')
               AND tweet_source = 'android' -- these are likely tweets coming from his device
-              AND text NOT LIKE '"@%' -- he often RTs lots of tweets in a row, and starts by copying the text of the tweet
+              AND is_trump_retweet != 1
               THEN 1 ELSE 0 END AS is_tweetstorm
 
 FROM c2;
