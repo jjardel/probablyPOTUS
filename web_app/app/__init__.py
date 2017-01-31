275a3ba76e0331370d9e02a1e25b8839e366671b
jjardel <johnjardel@gmail.com>
2017-01-30 23:38:19 -0600

    removing old code

--- web_app/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from ..config import basedir
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-
--- web_app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-
