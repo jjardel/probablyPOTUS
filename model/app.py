c63e4c7cd5e51c767a0efcd8906b1e5a178ac9b4
jjardel <johnjardel@gmail.com>
2017-01-31 19:21:43 -0600

    adding top-level logger

--- model/app.py
+++ model/app.py
@@ -1,10 +1,16 @@
 from model.src import Listener, AuthHandler
 from tweepy import Stream
+from lib.utils.lw import get_header, get_root_logger
 
-auth_handler = AuthHandler()
 
-listener = Listener()
-stream = Stream(auth=auth_handler.auth, listener=listener)
-stream.filter(follow=['25073877'], async=True)
+if __name__ == '__main__':
 
-print("listening...")
+    lg = get_root_logger()
+    get_header(lg, 'Opening stream to listen for Trump-like tweets.')
+    auth_handler = AuthHandler()
+
+    listener = Listener()
+    stream = Stream(auth=auth_handler.auth, listener=listener)
+    stream.filter(follow=['25073877'], async=True)
+
+    lg.info('listening...')
